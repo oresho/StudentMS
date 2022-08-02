8e8383067acc7f07e1e0c8ee48bb512c4de6da24
Joseph John <joseph.john.ali.2001@gmail.com>
2022-08-03 00:39:33 +0100

    Configured tests

--- home/tests.py
+++ home/tests.py
@@ -6,12 +6,14 @@ class TestCreateStudent(TestCase):
     @classmethod
     def setUpTestData(cls):
         test_student = Student.objects.create(
+            username="test_student",
             first_name="John",
             last_name="Doe",
             email="joseph@example.com",
             password="password",
         )
         test_teacher = Teacher.objects.create(
+            username="test_teacher",
             first_name="John",
             last_name="Doe",
             email="a@a.com",
@@ -19,17 +21,29 @@ class TestCreateStudent(TestCase):
         )
         test_course = Course.objects.create(
             name="Test Course",
-            teacher=test_teacher,
         )
         test_subject = Subject.objects.create(
             name="Test Subject",
             course=test_course,
+            teacher=test_teacher.teacherprofile,
         )
 
-        def test_student_profile_creation(self):
-            self.assertEqual(test_student.role, User.Role.STUDENT)
-            self.assertEqual(StudentProfile.objects.count(), 1)
-            self.assertEqual(StudentProfile.objects.first().student, test_student)
-            self.assertEqual(test_teacher.role, User.Role.TEACHER)
-            self.assertEqual(TeacherProfile.objects.count(), 1)
-            self.assertEqual(TeacherProfile.objects.first().teacher, test_teacher)
+    def test_student_profile_creation(self):
+        test_student = Student.student.get(id=1)
+        test_student_profile = StudentProfile.objects.get(student=test_student)
+        self.assertEqual(test_student.profile, test_student_profile)
+        self.assertEqual(test_student.welcome(), "Only for students")
+        self.assertEqual(test_student_profile.name, "")
+        self.assertEqual(test_student_profile.surname, "")
+        
+        # Teacher
+        test_teacher = Teacher.teacher.get(id=2)
+        test_teacher_profile = TeacherProfile.objects.get(teacher=test_teacher)
+        self.assertEqual(test_teacher_profile.name, None)
+        self.assertEqual(test_teacher_profile.surname, None)
+        self.assertEqual(test_student.role, User.Role.STUDENT)
+        self.assertEqual(StudentProfile.objects.count(), 1)
+        self.assertEqual(StudentProfile.objects.first().student, test_student)
+        self.assertEqual(test_teacher.role, User.Role.TEACHER)
+        self.assertEqual(TeacherProfile.objects.count(), 1)
+        self.assertEqual(TeacherProfile.objects.first().teacher, test_teacher)
