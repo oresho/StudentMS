009bb6ddf710de799964062c3233cab1580b8465
Joseph John <joseph.john.ali.2001@gmail.com>
2022-08-03 21:18:29 +0100

    Update

--- home/models.py
+++ home/models.py
@@ -4,12 +4,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# class Course(models.Model):
-#     course_code = models.CharField(max_length=20)
-
-#     def __str__(self):
-#         return self.course_code
-
 
 class User(AbstractUser):
     class Role(models.TextChoices):
